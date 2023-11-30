947fd56c4e573c63d50caea479e7360ded5aeb74
Andrehlb <andrehlb@me.com>
2023-11-30 14:59:16 -0300

    weather-api

--- weather.js
+++ weather.js
@@ -1,17 +1,20 @@
-// Substitua 'YOUR_API_KEY' pela chave da API que você obteve
 const apiKey = '5642e7d249c2bc5dd8dc90a9a7bed611';
 const city = 'Limeira';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},BR&appid=${apiKey}&units=metric`;
 
 function getWeather() {
     fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
             document.getElementById('temp').textContent = data.main.temp;
-            document.getElementById('condition').textContent = data.weather[0].description;
+            document.getElementById('condition').textContent = data.weather[0].main;
         })
         .catch(error => console.error('Erro ao obter dados da API:', error));
 }
 
-// Chama a função ao carregar a página
 window.onload = getWeather;
