dd81a24411189bc8aa571d8a9ceae46b1345c149
Andrehlb <andrehlb@me.com>
2023-12-01 16:16:36 -0300

    Set up static file serving for HTML and JS files.

--- weather-node/server.js
+++ weather-node/server.js
@@ -4,6 +4,8 @@ const apiKey = process.env.API_KEY;
 const express = require('express');
 const app = express();
 
+app.use(express.static('caminho_para_meus_arquivos_estaticos'));
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
