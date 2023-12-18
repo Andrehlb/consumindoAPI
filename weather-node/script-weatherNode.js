066df5acad03b21f08b84bc6f2b3014d0bd00ebc
Andrehlb <andrehlb@me.com>
2023-12-18 10:46:07 -0300

    Add eventlistener to fetch and display weather data on DOMContentLoaded in script-weatherNode.js

--- weather-node/script-weatherNode.js
+++ weather-node/script-weatherNode.js
@@ -1,1 +1,12 @@
-document.addEventListener()
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2c9d5b9b0b0b0b0b0b0b0b0b0b0b0b0b')
+    .then(response => response.json())
+    .then(data) => {
+        const weatherDiv = document.getElementById('Weather');
+        const content = `Temperatura: ${data.main.temp}°C<br>
+                         Clima: ${data.weather[0].main}<br>
+                         Descrição: ${data.weather[0].description}`;
+        weatherDiv.innerHTML = content;
+    })
+    .catch(error => console.error('Erro ao buscar dados: ', error));
+    }
