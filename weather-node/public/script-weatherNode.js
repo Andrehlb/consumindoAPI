cf4975a4948d8e28a33c5baf9a3f9acdb5a9dee6
Andrehlb <andrehlb@me.com>
2023-12-18 18:25:52 -0300

    Update script-weatherNode.js to fetch and display weather for default city on page load and for user-input city on button click.

--- weather-node/public/script-weatherNode.js
+++ weather-node/public/script-weatherNode.js
@@ -1,12 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/weather')
-    .then(response => response.json())
+function fetchWeather() {
+    fetch('/weather?city=${city}')
+    .then(reponse => response.json())
     .then(data => {
         const weatherDiv = document.getElementById('Weather');
-        const content = `Temperatura: ${data.main.temp}°C<br>
+        const content = `Cidade: ${city}<br>
+                         Temperatura: ${data.main.temp}°C<br>
                          Clima: ${data.weather[0].main}<br>
                          Descrição: ${data.weather[0].description}`;
         weatherDiv.innerHTML = content;
     })
     .catch(error => console.error('Erro ao buscar dados: ', error));
-    });
+}
+
+// Evento que carrega a previsão do tempo para uma cidade padrão ao carregar a página.
+document.addEventListener('DOMContentLoaded', function() {
+    const defaulttCity('Limeira');
+    fetchWeather('deafultCity')
+});
+
+// Evento de clique do botão que busca a previsão do tempo para a cidade inserida pelo usuário.
+document.getElementById('checkWeather').addEventListener('click', function() {
+    const city = document.getElementById('cityInput').value;
+    fetchWeather(city);
+});
