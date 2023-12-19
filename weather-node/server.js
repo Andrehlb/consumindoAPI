db1a551a4609fabfcbc1d73095d0af1943a1133c
Andrehlb <andrehlb@me.com>
2023-12-19 09:50:38 -0300

    Update script-weatherNode.js to fetch and display weather for default city on page load and for user-input city on button click.

--- weather-node/server.js
+++ weather-node/server.js
@@ -12,8 +12,8 @@ app.use(express.static('public'));
 // Rota para obter dados da API do OpenWeatherMap (ou meteorológicos)
 app.get('/weather', async (req, res) => {
     try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Limeira,BR&appid=${apiKey}&units=metric`); // Estático
-        // const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.query.city},BR&appid=${apiKey}&units=metric`); // Dinâmico
+        // const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Limeira,BR&appid=${apiKey}&units=metric`); // Estático
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.query.city},BR&appid=${apiKey}&units=metric`); // Dinâmico
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ message: error.message });
